9078e54869c0afaea2093d491515d78a67f670e6
LeoDog896 <LeoDog896@hotmail.com>
2021-10-21 19:07:45 -0400

    Ckeanup imports

--- src/main/kotlin/world/cepi/atlas/AtlasExtension.kt
+++ src/main/kotlin/world/cepi/atlas/AtlasExtension.kt
@@ -1,15 +1,11 @@
 package world.cepi.atlas
 
-import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.extensions.Extension
-import net.minestom.server.instance.block.Block
 import net.minestom.server.instance.block.BlockHandler
 import world.cepi.atlas.commands.AtlasCommand
 import world.cepi.atlas.handler.SignHandler
 import world.cepi.atlas.handler.SkullHandler
 import world.cepi.kstom.Manager
-import world.cepi.kstom.command.register
-import world.cepi.kstom.command.unregister
 import world.cepi.kstom.event.listenOnly
 
 class AtlasExtension : Extension() {
